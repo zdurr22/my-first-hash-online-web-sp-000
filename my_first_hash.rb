002d1f6c2fe44f9f384334883467e9b6ced5106c
Zachary Durr <zdurr22@gmail.com>
2020-10-06 23:02:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = {
+  "my name" => "Zachary Durr"
+  "my job" => "Project Manager at MJ Daly"
+}
   
 end
 
